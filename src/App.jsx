639edee44d6567e5d399bb4f4bdd8f6f221156cc
Marina <98743748+MRudkoo@users.noreply.github.com>
2023-12-14 01:46:43 +0200

    Add files via upload
    Section "About" has been created

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,38 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import About from './components/About';
-import Error from './components/Error';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Important from './components/Important';
-import PartOne from './components/PartOne';
-import PartTwo from './components/PartTwo';
-import RedBlock from './components/RedBlock';
-import Stories from './components/Stories';
+import About from "./components/About";
+import Error from "./components/Error";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import PartOne from "./components/PartOne";
+import PartTwo from "./components/PartTwo";
+import RedBlock from "./components/RedBlock";
+import Stories from "./components/Stories";
+import WhiteBlock from "./components/WhiteBlock";
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: (
-			<>
-				<Header />
-				<Hero />
-				<About />
-				<PartOne />
-				<Stories />
-				<PartTwo/>
-				<RedBlock/>
-				<Important/>
-				<Footer/>
-			</>
-		),
-		errorElement: (
-			<>
-				<Header />
-				<Error />
-			</>
-		
-		)
-	},
+  {
+    path: "/",
+    element: (
+      <>
+        <Header />
+        <Hero />
+        <About />
+        <PartOne />
+        <Stories />
+        <PartTwo />
+        <RedBlock />
+        <WhiteBlock />
+        <Footer />
+      </>
+    ),
+    errorElement: <Error />,
+  },
 ]);
 
 const App = () => {
-	return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
