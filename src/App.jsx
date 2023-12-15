6d1234c1366716e33f72a1d8c0993b37fb46a93e
Viktor Filippov <jackal3166@gmail.com>
2023-12-16 00:48:33 +0200

    Fix Important

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import About from "./components/About";
-import Error from "./components/Error";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import PartOne from "./components/PartOne";
-import PartTwo from "./components/PartTwo";
-import RedBlock from "./components/RedBlock";
-import Stories from "./components/Stories";
-import WhiteBlock from "./components/WhiteBlock";
+import About from './components/About';
+import Error from './components/Error';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Important from './components/Important';
+import PartOne from './components/PartOne';
+import PartTwo from './components/PartTwo';
+import RedBlock from './components/RedBlock';
+import Stories from './components/Stories';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <Header />
-        <Hero />
-        <About />
-        <PartOne />
-        <Stories />
-        <PartTwo />
-        <RedBlock />
-        <WhiteBlock />
-        <Footer />
-      </>
-    ),
-    errorElement: <Error />,
-  },
+	{
+		path: '/',
+		element: (
+			<>
+				<Header />
+				<Hero />
+				<About />
+				<PartOne />
+				<Stories />
+				<PartTwo />
+				<RedBlock />
+				<Important />
+				<Footer />
+			</>
+		),
+		errorElement: (
+			<>
+				<Header />
+				<Error />
+			</>
+		),
+	},
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 };
 
 export default App;
