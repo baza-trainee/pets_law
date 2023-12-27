9d7def752a36cf6a5f343f3d077baba1c22ecc7a
m0o0n <strikunvlad@gmail.com>
2023-12-27 18:16:29 +0200

    bug: CL-12 CL-13 resolved

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 import Logo from "../assets/logo.svg?react";
-import { Link } from "react-scroll";
+import Link from "../components/common/Link";
 import { IoIosArrowDown } from "react-icons/io";
 import { useState } from "react";
+import ButtonDonate from "./common/ButtonDonate";
 
 const Footer = () => {
   const [isMenu, setMenu] = useState(false);
@@ -16,9 +17,14 @@ const Footer = () => {
 
           <nav>
             <ul className="flex flex-col gap-[10px] w-fit">
-              <li className="w-fit text-white font-extralight text-[14px] leading-[16px] cursor-pointer">
-                <Link duration={500} to="hero">
+              <li className="w-fit text-white font-extralight text-[14px] leading-[16px] cursor-pointer transition-all duration-[300ms] hover:text-black">
+                <Link to="hero">
                   Головна
+                </Link>
+              </li>
+              <li className="w-fit text-white font-extralight text-[14px] leading-[16px] cursor-pointer transition-all duration-[300ms] hover:text-black">
+                <Link to="about">
+                  Про нас
                 </Link>
               </li>
               <li className="text-white font-extralight text-[14px] leading-[16px] cursor-pointer">
@@ -27,11 +33,11 @@ const Footer = () => {
                     setMenu(!isMenu);
                   }}
                   type="button"
-                  className="flex items-center gap-[8px] hover:text-black"
+                  className="flex items-center gap-[8px] transition-all duration-[300ms] hover:text-black"
                 >
                   <span>Права тварин</span>
                   <IoIosArrowDown
-                    className="mf-[8px] transition-all duration-[300ms]"
+                    className="mf-[8px]"
                     style={{
                       transform: isMenu ? "rotate(180deg)" : "rotate(0deg)",
                     }}
@@ -47,7 +53,6 @@ const Footer = () => {
                 <ul className="flex flex-col gap-[10px] w-fit">
                   <li>
                     <Link
-                      duration={500}
                       to="part_1"
                       className="block w-fit text-white font-extralight text-[14px] leading-[16px] cursor-pointer hover:text-black"
                     >
@@ -56,7 +61,6 @@ const Footer = () => {
                   </li>
                   <li>
                     <Link
-                      duration={500}
                       to="part_2"
                       className="block w-fit text-white font-extralight text-[14px] leading-[16px] cursor-pointer hover:text-black"
                     >
@@ -65,13 +69,13 @@ const Footer = () => {
                   </li>
                 </ul>
               </li>
-              <li className="w-fit text-white font-extralight text-[14px] leading-[16px] cursor-pointer">
-                <Link duration={500} to="suggest">
+              <li className="w-fit text-white font-extralight text-[14px] leading-[16px] cursor-pointer transition-all duration-[300ms] hover:text-black">
+                <Link to="suggest">
                   Поради
                 </Link>
               </li>
-              <li className="w-fit text-white font-extralight text-[14px] leading-[16px] cursor-pointer">
-                <Link duration={500} to="partners">
+              <li className="w-fit text-white font-extralight text-[14px] leading-[16px] cursor-pointer transition-all duration-[300ms] hover:text-black">
+                <Link to="partners">
                   Партнери
                 </Link>
               </li>
@@ -84,30 +88,30 @@ const Footer = () => {
           </nav>
         </div>
         <div className="flex flex-col items-end pt-[48px] pb-[36px]">
-          <button
-            type="button"
-            className="w-[341px] h-[44px] p-[10px] mb-[20px] rounded-[20px] bg-white text-[14px] leading-[16px] hover:bg-yellow focus:bg-yellow duration-200 hover:shadow-[0px_4px_4px_0px_rgba(0,0,0,0.10)]"
-          >
-            Підтримати проєкт
-          </button>
-          <p className="text-base leading-5 text-white">
+
+        <ButtonDonate className="text-center w-[341px] h-[44px] p-[10px] mb-[20px] rounded-[20px] bg-white text-[20px] leading-[16px] hover:bg-yellow focus:bg-yellow duration-200 hover:shadow-[0px_4px_4px_0px_rgba(0,0,0,0.10)] cursor-pointer"/>
+          <p className="leading-5 text-white text-body">
             Розробка BazaTraineeUkraine 2023 &copy; Усі права захищені
           </p>
         </div>
       </div>
 
       {/* Mobile */}
-      <div className="relative block w-full h-full mx-auto pt-[30px] pb-[10px] px-[20px] lg:hidden ">
+      <div className="relative block w-full h-full mx-auto pt-[50px] pb-[10px] px-[25px] lg:hidden ">
         <div className="flex justify-between">
           <nav>
             <ul className="flex flex-col gap-[10px] w-fit mb-[19px]">
               <li className="w-fit text-white font-extralight text-[14px] leading-[16px] cursor-pointer hover:text-black">
                 <Link
-                  duration={500}
                   to="hero"
-                  aria-label="Move to Goal section"
                 >
                   Головна
+                </Link>
+              </li>
+
+              <li className="w-fit text-white font-extralight text-[14px] leading-[16px] cursor-pointer hover:text-black">
+                <Link to="about">
+                  Про нас
                 </Link>
               </li>
               <li className="text-white font-extralight text-[14px] leading-[16px] cursor-pointer">
@@ -116,11 +120,11 @@ const Footer = () => {
                     setMenu(!isMenu);
                   }}
                   type="button"
-                  className="flex items-center gap-[8px] hover:text-black"
+                  className="flex items-center gap-[8px] hover:text-black  transition-all duration-[300ms]"
                 >
                   <span>Права тварин</span>
                   <IoIosArrowDown
-                    className="mf-[8px] transition-all duration-[300ms]"
+                    className="mf-[8px]"
                     style={{
                       transform: isMenu ? "rotate(180deg)" : "rotate(0deg)",
                     }}
@@ -145,7 +149,6 @@ const Footer = () => {
                   </li>
                   <li>
                     <Link
-                      duration={500}
                       to="part_2"
                       className="block w-fit text-white font-extralight text-[14px] leading-[16px] cursor-pointer hover:text-black"
                     >
@@ -154,7 +157,6 @@ const Footer = () => {
                   </li>
                   <li>
                     <Link
-                      duration={500}
                       to="part_3"
                       className="block w-fit text-white font-extralight text-[14px] leading-[16px] cursor-pointer hover:text-black"
                     >
@@ -164,12 +166,12 @@ const Footer = () => {
                 </ul>
               </li>
               <li className="w-fit text-white font-extralight text-[14px] leading-[16px] cursor-pointer hover:text-black">
-                <Link duration={500} to="suggest">
+                <Link to="suggest">
                   Поради
                 </Link>
               </li>
               <li className="w-fit text-white font-extralight text-[14px] leading-[16px] cursor-pointer hover:text-black">
-                <Link duration={500} to="partners">
+                <Link to="partners">
                   Партнери
                 </Link>
               </li>
